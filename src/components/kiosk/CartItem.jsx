b52477a27e6d6ed091eecd090a21a53aa7f2e058
pyouji99@naver.com <parkbori99@gmail.com>
2026-07-15 10:16:17 +0900

    장버구니컴포넌트 옴션추가시 보여지게 수정

--- src/components/kiosk/CartItem.jsx
+++ src/components/kiosk/CartItem.jsx
@@ -1,4 +1,6 @@
 function CartItem({ item, onIncrease, onDecrease, onRemove }) {
+  const hasOptions = item.options.length > 0; // 옵션 있는 메뉴인지 확인
+
   const optionTotal = item.options.reduce((sum, o) => sum + o.option_price, 0);
   const itemTotal = (item.base_price + optionTotal) * item.quantity;
 
@@ -6,7 +8,21 @@ function CartItem({ item, onIncrease, onDecrease, onRemove }) {
     <div>
       <img src={item.image_url} alt={item.menu_name} />
 
-      <p>{item.menu_name}</p>
+      <div>
+        <p>{item.menu_name}</p>
+
+        {/* 옵션이 있을 때만 렌더링 */}
+        {hasOptions && (
+          <div>
+            {item.options.map((option) => (
+              <div key={option.option_id}>
+                <span>+{option.option_name}</span>
+                <span>+{option.option_price.toLocaleString()}원</span>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
 
       <div>
         <button type="button" onClick={onDecrease}>
